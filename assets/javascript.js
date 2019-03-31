8dc4586347b1d9c40a59f03f09e26da1c9fc4a75
Dylan Davidson <rogue1037@gmail.com>
2019-03-31 16:36:19 -0700

    fixed HTML

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,1 +1,34 @@
-// Here is our javascript file
+$(document).ready(function() {
+    event.preventDefault();
+      console.log( "ready!" );
+  var queryURL = 'https://maps.googleapis.com/maps/api/geocode/json?address=UK&key=AlzaSyDLPVgCgH_W_SZTklXhTP8irhCfdzB90wk';
+  
+      $.ajax({
+        url: queryURL, 
+        type: 'GET',
+        success: function(response) {
+          console.log(response);
+        }
+    });
+  
+   
+  });
+  
+  
+   
+  function initMap(){
+    // Map options
+    var options = {
+        zoom: 8, // starting zoom of map
+        center:{lat:42.3601, lng:-71.0589} // boston need to change to input of search
+    }
+        // New Map
+    var map = new
+    google.maps.Map(document.getElementById('#map'),options);
+  
+    // add marker
+    var marker = new google.maps.Marker({
+        position:{lat:42.4668, lng:-70.9495},
+        map:map,
+    });
+  }
