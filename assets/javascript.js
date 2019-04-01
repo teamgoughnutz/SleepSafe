b5c935e6ddc99687bb804ac0b67af4f0c5a408bf
Dylan Davidson <rogue1037@gmail.com>
2019-04-01 13:48:16 -0700

    added initMap back.

--- assets/javascript.js
+++ assets/javascript.js
@@ -12,20 +12,20 @@ function initMap() {
 }
 
 
-  // var initMap = function(){
-  //   // Map options
-  //   var options = {
-  //       zoom: 8, // starting zoom of map
-  //       center:{lat:42.3601, lng:-71.0589} // boston need to change to input of search
-  //   };
-  //       // New Map
-  //   var map = new google.maps.Map(document.getElementById('map'),options);
+  var initMap = function(){
+    // Map options
+    var options = {
+        zoom: 8, // starting zoom of map
+        center:{lat:42.3601, lng:-71.0589} // boston need to change to input of search
+    };
+        // New Map
+    var map = new google.maps.Map(document.getElementById('map'),options);
   
-  //   // add marker
-  //   var marker = new google.maps.Marker({
-  //       position:{lat:42.4668, lng:-70.9495},
-  //       map:map,
-  //   });
-  //   console.log(initMap);
+    // add marker
+    var marker = new google.maps.Marker({
+        position:{lat:42.4668, lng:-70.9495},
+        map:map,
+    });
+    console.log(initMap);
     
-  // };
+  };
